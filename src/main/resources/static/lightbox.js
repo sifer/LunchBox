ce31cd4db1929b75b7f1de455d37840698b6ce94
Nadiamef <nadia.mefroum@gmail.conm>
2017-03-30 15:23:04 +0200

    Nadias Versioooon

--- src/main/resources/static/lightbox.js
+++ src/main/resources/static/lightbox.js
@@ -11,15 +11,9 @@ function ready(fn) {
 function fn() {
     var element =  document.querySelector("#annons");
     element.style.visibility = 'visible';
-    var element2 =  document.querySelector("#black_overlay");
-    element2.style.visibility = 'visible';
+ /*   var element2 =  document.querySelector("#black_overlay");
+    element2.style.visibility = 'visible';*/
 }
-
-
-
-
-
-
 
 
 function hej(fa) {
@@ -35,4 +29,14 @@ function fa() {
     elementt.style.visibility = 'visible';
     var elementt2 =  document.querySelector("#black_overlay");
     elementt2.style.visibility = 'visible';
+}
+
+
+function closeFn(){
+    var element =  document.querySelector("#annons");
+    element.style.visibility = 'hidden';
+}
+function closeFa(){
+    var element =  document.querySelector("#bg");
+    element.style.visibility = 'hidden';
 }
